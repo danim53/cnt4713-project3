6635ceed9443c3882ead835ac2f32d8e610d9ba6
migdo <mrome182@fiu.edu>
2026-04-13 20:23:37 -0400

    name File to Server- Migdony Romero

--- Server.py
+++ Server.py
@@ -1,3 +1,28 @@
 import socket
-import threading
+from idlelib.undo import Command
+import rsa
 import hashlib
+from multiprocessing import connection
+
+HOST ="127.0.0.1"
+CONTROL_PORT = 8080
+
+print("Starting Server...")
+print("Creating RSA keypair")
+public_key, private_key = rsa.newkeys(512)
+print("RSA keypair created")
+
+print("Creating server scoket")
+server = socket.socket()
+server.bind((HOST, CONTROL_PORT))
+server.listen()
+
+print("Awaiting connection...")
+connect, addr = server.accept()
+
+# command
+command = con.recv(1024).decode()
+
+if command == "CONNECT":
+    print("Connection requested. Creating data soket")
+    data_server = socket.socket(
